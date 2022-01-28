f4da3aee30b6388cd01d690ee98be096cb84b21d
SnO2WMaN <me@sno2wman.net>
2022-01-28 16:58:49 +0900

    refactoring procss commits

--- src/engine/commits.ts
+++ src/engine/commits.ts
@@ -4,19 +4,18 @@ export type Commit = InsertCommitType | DeleteCommitType;
 import { insertCommit, InsertCommitType } from "./insert_commit.ts";
 import { deleteCommit, DeleteCommitType } from "./delete_commit.ts";
 
-export const commitReducer = (lines: LineType[], commit: Commit): LineType[] => {
-  switch (commit.method) {
-    case "INSERT":
-      return insertCommit(lines, commit.payload);
-    case "DELETE":
-      return deleteCommit(lines, commit.payload);
-    default:
-      return lines;
-  }
-};
 export const processCommits = (lines: LineType[], commits: Commit[]) => {
   for (const commit of commits) {
-    lines = commitReducer(lines, commit);
+    switch (commit.method) {
+      case "INSERT":
+        lines = insertCommit(lines, commit.payload);
+        break;
+      case "DELETE":
+        lines = deleteCommit(lines, commit.payload);
+        break;
+      default:
+        break;
+    }
   }
   return lines;
 };
