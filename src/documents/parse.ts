9e65c80920259023d6e0d1737af838335e5d9da4
SnO2WMaN <me@sno2wman.net>
2022-01-29 18:30:45 +0900

    remove parse lines

--- src/documents/parse.ts
+++ src/documents/parse.ts
@@ -1,21 +1,10 @@
 import { documents } from "./storage.ts";
 import { CommitUnion, DocumentType, LineType, ResponseDocument, ResponseDocumentLine } from "../types.ts";
 
-export const parseLines = (lines: LineType[], firstLineId: string): ResponseDocumentLine[] => {
-  const firstLine = lines.find((line) => line.lineId === firstLineId);
-  if (!firstLine) throw new Error("Something broken");
-  const sorted = [firstLine];
-  for (let i = 0; i < lines.length; i++) {
-    const nextLine = lines.find((line) => line.lineId === sorted[sorted.length - 1].nextLineId);
-    if (!nextLine) break;
-    sorted.push(nextLine);
-  }
-  return sorted.map(({ lineId, text }) => ({ lineId, text }));
-};
 export const parse = (document: DocumentType): ResponseDocument => {
   return {
     id: document.id,
-    lines: parseLines(document.lines, document.firstLineId),
+    lines: document.lines,
     latestCommitId: document.latestCommitId,
   };
 };
