b157a69ac694d104db6b53a2ab6cec1960c49432
SnO2WMaN <me@sno2wman.net>
2022-02-05 11:03:05 +0900

    simplify line

--- src/documents_2/simplify_content.ts
+++ src/documents_2/simplify_content.ts
@@ -1,7 +1,20 @@
 import { DocumentContent, LinesMap } from "./types.ts";
 
-export const simplifyLinesMap = (lines: LinesMap): { id: string; text: string }[] => {
-  return [];
+export const simplifyLinesMap = (map: LinesMap): { id: string; text: string }[] => {
+  const lines = [...map.entries()].map(([lineId, rest]) => ({ lineId, ...rest }));
+  const firstLine = lines.find(({ prevLineId }) => prevLineId === null);
+  if (!firstLine) throw new Error("First line does not exist");
+
+  const sorted = [firstLine];
+  for (let i = 0; i < lines.length - 1; i++) {
+    const nextLineId = sorted[i].postLineId;
+    if (!nextLineId) throw new Error("Next line id does not exist");
+    const nextLine = lines.find(({ lineId }) => lineId === nextLineId);
+    if (!nextLine) throw new Error("Next line does not exist");
+    sorted.push(nextLine);
+  }
+
+  return sorted.map(({ lineId, text }) => ({ text, id: lineId }));
 };
 
 export const simplifyContent = (content: DocumentContent) => {
